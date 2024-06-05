60b950d9ada6deb347a4a6d0f9313b284ede6b1e
Antonio <110134717+Antoniomv7@users.noreply.github.com>
2024-06-05 17:27:52 +0200

    Add files via upload

---
+++ 1-Tests/Second-phase/addmul_filter/addmul.cpp
@@ -0,0 +1,10 @@
+void addmul(int a, int b, int& c, int& m) {
+#pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS INTERFACE s_axilite port=a
+#pragma HLS INTERFACE s_axilite port=b
+#pragma HLS INTERFACE s_axilite port=c
+#pragma HLS INTERFACE s_axilite port=m
+
+    c = a + b;
+	m = a * b;
+}
